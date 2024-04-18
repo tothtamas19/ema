270a09eb057d631297238d927ef3f5d74dfe9494
Juhász Zsombor <juhasz.zsombor12@gmail.com>
2024-04-18 10:26:15 +0200

    A create művelet kész

--- web/src/app.js
+++ web/src/app.js
@@ -1,16 +1,29 @@
 const doc = {
     empBody: document.querySelector("#empBody"),
-    addButton: document.querySelector("#addButton")
+    addButton: document.querySelector("#addButton"),
+    nameInput: document.querySelector("#name"),
+    cityInput: document.querySelector("#city"),
+    salaryInput: document.querySelector("#salary")
 }
 
 const state = {
-    url :'http://localhost:8000/employees'
+    url :'http://localhost:8000/employees',
+    neme: 'névtelen',
+    city: 'ismeretlen',
+    salary: 300
 }
 
 doc.addButton.addEventListener('click', () => {
     console.log("működik")
+    getDataFromFrom()
     createEmployee()
 })
+
+function getDataFromFrom(){
+    state.name = doc.nameInput.value
+    state.city = doc.cityInput.value
+    state.salary = doc.salaryInput.value
+}
 
 function createEmployee() {
     fetch(state.url, {
@@ -19,9 +32,9 @@ function createEmployee() {
             "Content-Type":"application/json"
         },
         body: JSON.stringify( {
-            name: "Valaki",
-            city: "Valahol",
-            salary: 300
+            name: state.name,
+            city: state.city,
+            salary: state.salary
 
         })
     })
